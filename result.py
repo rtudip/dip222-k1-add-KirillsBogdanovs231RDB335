8098094c6aca1b1880a0130f2e883d0dae93bbd7
KirillsBogdanovs231RDB335 <144994793+KirillsBogdanovs231RDB335@users.noreply.github.com>
2023-12-15 10:02:31 +0000

    15.12.2023.

---
+++ result.py
@@ -0,0 +1,71 @@
+#1.uzd 
+#import pandas
+#with open("data.csv","r") as f:
+    #next(f)
+
+    #time =[]
+    #regi = "Oman"
+    #for line in f:
+        #row=line.rstrip().split(",")
+        #if row[4] == regi:
+            #time.append(int(row[6]))
+    #print(min(time))
+
+#2.uzd
+#import pandas
+#with open("data.csv","r") as f:
+    #next(f)
+    #h=[]
+    #regi = "Latvia"
+    #for line in f:
+        #row=line.rstrip().split(",")
+        #if row[4]==regi:
+            #h.append(int(row[8]))
+    #print(sum(f))
+#3.uzd
+
+#import pandas
+#with open("data.csv","r") as f:
+    #next(f)
+    #count=[]
+    #regi = "Latvia"
+    #inds = "Telecommunications"
+    #for a in f:
+        #a=a.rstrip().split(",")
+        #if x[4] == regi:
+            #if x[7] == inds:
+                #count.append(x[8])
+    #print(count)
+#4.uzd
+#import pandas
+#with open("data.csv","r") as f:
+    #next(f)
+    #link=[]
+    #regi = "Latvia"
+    #for line in f:
+        #row=line.rstrip.split(",")
+        #if row[4] == regi:
+            #link.append(row[3])
+    #s = "https://"
+    #b = []
+    #for g in link:
+        #if s in g:
+            #b.append(g)
+    #print(len(b))
+
+#5.uzd
+#import pandas
+#with open("data.csv","r") as f:
+    #next(f)
+    #link = []
+    #regi = "Latvia"
+    #for line in f:
+        #row=line.rstrip.split(",")
+        #if row[4] == regi:
+            #link.append(row[3])
+    #s = ".org"
+    #n = []
+    #for g in link:
+        #if s in g:
+           # n.append(g)
+    #print(len(n))
